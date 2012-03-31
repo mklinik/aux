a21bc1b7c0bf04e6f41bc29e6208dfde2902987b
Markus Klinik <markus.klinik@gmx.de>
2012-03-31 16:19:39 +0200

    embed.sh: more notes for n900 users

--- embed.sh
+++ embed.sh
@@ -2,22 +2,30 @@
 
 # embed an image into the meta-data of an audio file
 
-# supports mp3 and ogg, and jpeg images
+# supports mp3 and ogg audio files, and jpeg images
 
-# prerequisites:
+# Prerequisites:
 #  eyeD3 (for mp3 support)
 #  vorbis-tools (for ogg support)
 
-# example:
+# Example:
 #  $ ls
 #  01-rancid-indestructible.ogg  cover.jpg
 #  $ embed.sh 01-rancid-indestructible.ogg
 
-# Note for n900 users:
+# Notes for n900 users:
+#
 # - The newest version of the n900 media player ignores cover.jpg and
 # folder.jpg, instead it needs cover art embedded in the audio file.
+#
 # - The n900 media player only needs the cover embedded in the first file of an
 # album to correctly display the album art for all tracks of the album
+#
+# - The n900 media player needs the cover art for mp3 files embedd in the tag
+# OTHER. If an mp3 file already has an image embedded, but in a different tag,
+# say FRONT_COVER (find out with: eyeD3 <audio-file>), do the following:
+#  $ eyeD3 -i . <audio-file> # saves all embedded images to the current directory
+#  $ embed.sh <audio-file> FRONT_COVER.jpg
 
 function usage()
 {
