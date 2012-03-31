869d01622b68f5ab5ac553d67b85e519c3bd4f4b
Markus Klinik <markus.klinik@gmx.de>
2012-03-31 18:47:17 +0200

    embed: even more n900 blah

--- embed.sh
+++ embed.sh
@@ -15,17 +15,38 @@
 
 # Notes for n900 users:
 #
-# - The newest version of the n900 media player ignores cover.jpg and
-# folder.jpg, instead it needs cover art embedded in the audio file.
+# - For OGG files, the newest version of the n900 media player ignores
+# cover.jpg and folder.jpg, instead it needs cover art embedded in the ogg
+# file. For mp3 files, cover.jpg works fine. This is probably because the
+# tracker-extractor for ogg is a different program (you have to install an
+# additional package) and they forgot to add support for cover.jpg.
 #
 # - The n900 media player only needs the cover embedded in the first file of an
 # album to correctly display the album art for all tracks of the album
 #
-# - The n900 media player needs the cover art for mp3 files embedd in the tag
-# OTHER. If an mp3 file already has an image embedded, but in a different tag,
-# say FRONT_COVER (find out with: eyeD3 <audio-file>), do the following:
-#  $ eyeD3 -i . <audio-file> # saves all embedded images to the current directory
-#  $ embed.sh <audio-file> FRONT_COVER.jpg
+# - Album art for mp3 files works with cover.jpg and embedded (I have tried the
+# tags FRONT_COVER and OTHER, both work). Other filenames work, too (I have
+# tried FRONT_COVER.jpg) It's just those darn mp3 files that 7digital
+# distributes that don't work. The cached files in .mediaartlocal and
+# ~/.cache/media-art are broken for some reason. file says: 'data'. My guess is
+# the file comment that 7digital includes, has backslashes.
+#
+# Hence, for mp3 files from 7digital, do the following BEFORE copying the files
+# to the n900:
+#  $ eyeD3 -i . <first-audio-file>
+#  $ eyeD3 --remove-images *.mp3
+#
+# Or, if you already have broken images in your cache, do the following on your PC:
+#  # use sshfs to mount your n900 home directory
+#  $ file n900/.cache/media-art | grep data # find all garbled files in the global cache
+#  # delete those files
+#  $ cd n900/MyDocs/<problematic-album>
+#  $ rm -r .mediaartlocal # delete local cache
+#  $ eyeD3 -i . <first-audio-file> # extract FRONT_COVER.jpg, which the n900 recognizes
+#  $ eyeD3 --remove-images *.mp3
+#  # count to three, monty python style (to give the indexer time to notice
+#  that something has changed), and restart the media player
+
 
 function usage()
 {
