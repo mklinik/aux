eeed64765158a1d23bcff0daf380c95551f673e1
Markus Klinik <mkl@lambdanaut.net>
2020-01-06 15:03:30 +0100

    7digital: better error handling

--- fix-7digital-tags.sh
+++ fix-7digital-tags.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-eyeD3 -i . 01*
+if [ $# -lt 1 ]
+then
+  echo "usage: $0 <file.mp3>"
+  echo "Extracts embedded image from given song; removes all images; adds image to all songs."
+  exit 0
+fi
+
+eyeD3 -i . $1
 eyeD3 --remove-images *.mp3
 eyeD3 --add-image=FRONT_COVER.jpeg:FRONT_COVER *.mp3
